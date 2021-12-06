b4a4935aacb6517d625bb8e17adff50a0535912d
Aryan Singh <aryansunny285@gmail.com>
2021-12-06 21:37:22 +0530

    Update DuplicateElements.cpp

--- Array Problems/DuplicateElements.cpp
+++ Array Problems/DuplicateElements.cpp
@@ -2,5 +2,25 @@
 using namespace std;
 
 int findDuplicate(int *arr,int size){
-    
+    int ans=0;
+    for(int i=0;i<size;i++){
+        ans=ans^arr[i];
+    }
+    for(int i=1;i<size;i++){
+        ans=ans^i;
+    }
+    return ans;
+}
+
+int main(){
+    int size;
+    cout<<"Enter Size : ";
+    cin>>size;
+    int arr[100];
+    for(int i = 0; i<size;i++)
+    cin>>arr[i];
+    cout<<"The ans is : ";
+    int n = findDuplicate(arr,size);
+    cout<<n<<endl;
+    return 0;
 }
